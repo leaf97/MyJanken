1136089f5682bdbdc89337fe7c2300fdbfaae477
[leaf97] <[leafidentity7@gamil.com]>
2019-06-10 21:24:19 +0900

    インデント整理（コミット練習)

--- MyJanken/ViewController.swift
+++ MyJanken/ViewController.swift
@@ -28,32 +28,32 @@ class ViewController: UIViewController {
 //新しいじゃんけんの結果を一時的に格納する変数を設ける
         var newAnswerNumber = 0
 
-    //ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+//ランダムに結果を出すが、前回の結果と異なる場合のみ採用
 //repeatは繰り返しを意味する
         repeat {
             
-//         0.1.2の乱数をランダムで算出（乱数）
-// arc4random_uniform()の戻り値はUInt32だがSwihtの標準的な整数型Intにキャスト（変換）する
-            newAnswerNumber = Int(arc4random_uniform(3))
+// 0.1.2の乱数をランダムで算出する乱数）
+//arc4random_uniform()の戻り値はUInt32だがSwihtの標準的な整数型Intにキャスト（変換）する
+        newAnswerNumber = Int(arc4random_uniform(3))
             
             
 //前回と同じ結果の時は再度ランダムに数値を出す
 //異なるか結果の時は、repeat を抜ける
         } while answerNumber == newAnswerNumber
         
-        answerNumber = newAnswerNumber
+    answerNumber = newAnswerNumber
         
-        if answerNumber == 0 {
-//        グー
-        answerLabel.text = "グー"
-        answerImageView.image = UIImage(named:"gu")
+    if answerNumber == 0 {
+// グー
+    answerLabel.text = "グー"
+    answerImageView.image = UIImage(named:"gu")
     } else if answerNumber == 1 {
-//    チョキ
+// チョキ
     answerLabel.text = "チョキ"
     answerImageView.image = UIImage(named: "choki")
     } else if answerNumber == 2 {
 // パー
-answerLabel.text = "パー"
+    answerLabel.text = "パー"
     answerImageView.image = UIImage(named: "pa")
     }
 
